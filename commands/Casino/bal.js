b99d8add5fd0e8d3578acbad6eedb100587ac16d
TheDarkk5 <90706145+TheDarkk5@users.noreply.github.com>
2024-08-06 02:07:47 +0200

    Update bal.js

--- commands/Casino/bal.js
+++ commands/Casino/bal.js
@@ -1,7 +1,7 @@
 const db = require('quick.db');
 
 module.exports = {
-    name: 'bal',
+    name: 'balance',
     description: 'Check your balance',
     async execute(client, message, args) {
         let balance = db.get(`balance_${message.author.id}`) || 0;
