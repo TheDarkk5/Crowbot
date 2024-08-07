59d6bfc1ae19f043919b94836684b78cd03f7407
TheDarkk5 <90706145+TheDarkk5@users.noreply.github.com>
2024-08-07 23:22:24 +0200

    Update bal.js

--- commands/Casino/bal.js
+++ commands/Casino/bal.js
@@ -5,7 +5,7 @@ const { MessageActionRow, MessageButton, MessageMenuOption, MessageMenu } = requ
 module.exports = {
     name: 'balance',
     description: 'Check your balance',
-    async execute(client, message, args) {
+    run :async (client, message, args, prefix, color) => {
         let balance = db.get(`balance_${message.author.id}`) || 0;
         message.channel.send(`${message.author.username}, you have ${balance} coins.`);
     }
