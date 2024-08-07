24bb2f56d3aed2e97454fbb280e734c00628d7b1
TheDarkk5 <90706145+TheDarkk5@users.noreply.github.com>
2024-08-07 23:50:16 +0200

    Create daily.js

---
+++ commands/Casino/daily.js
@@ -0,0 +1,18 @@
+Discord = require('discord.js')
+const db = require('quick.db')
+const { MessageActionRow, MessageButton, MessageMenuOption, MessageMenu } = require('discord-buttons');
+
+module.exports = {
+    name: 'daily',
+    description: 'Claim your daily coins',
+    run : async(client, message, args, prefix, color) => {
+        let cooldown = db.get(`daily_${message.author.id}`);
+        if (cooldown && Date.now() - cooldown < 86400000) {
+            message.channel.send('You have already collected your daily coins. Come back later!');
+        } else {
+            db.set(`daily_${message.author.id}`, Date.now());
+            db.add(`balance_${message.author.id}`, 100);
+            message.channel.send(`You received your daily 100 coins, ${message.author.username}!`);
+        }
+    }
+};
